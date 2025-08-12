433df3a158e2117e82419cbef6e951ad9e53a2e9
huadi <292801371@qq.com>
2025-08-12 10:49:00 +0800

    feat: add knox

--- bigtop-packages/src/common/knox/install_knox.sh
+++ bigtop-packages/src/common/knox/install_knox.sh
@@ -27,6 +27,7 @@ usage: $0 <options>
   Optional options:
      --lib-dir=DIR               path to install knox home [/usr/lib/knox]
      --version=VERSION           knox version [2.0.0]
+     --etc-knox=DIR             path to install knox conf [/etc/knox]
      ... [ see source for more similar options ]
   "
   exit 1
@@ -38,6 +39,7 @@ OPTS=$(getopt \
   -l 'prefix:' \
   -l 'lib-dir:' \
   -l 'version:' \
+  -l 'etc-knox:' \
   -l 'build-dir:' -- "$@")
 
 if [ $? != 0 ] ; then
@@ -59,6 +61,9 @@ while true ; do
         --version)
         VERSION=$2 ; shift 2
         ;;
+        --etc-knox)
+        ETC_KNOX=$2 ; shift 2
+        ;;
         --)
         shift ; break
         ;;
@@ -79,17 +84,17 @@ done
 
 LIB_DIR=${LIB_DIR:-/knox}
 VERSION=${VERSION:-2.0.0}
+ETC_KNOX=${ETC_KNOX:-/etc/knox}
 
 install -d -m 0755 $PREFIX/$LIB_DIR
 install -d -m 0755 $PREFIX/$LIB_DIR/bin
-install -d -m 0755 $PREFIX/$LIB_DIR/conf
-install -d -m 0755 $PREFIX/$LIB_DIR/conf/topologies
 install -d -m 0755 $PREFIX/$LIB_DIR/data
 install -d -m 0755 $PREFIX/$LIB_DIR/dep
 install -d -m 0755 $PREFIX/$LIB_DIR/ext
 install -d -m 0755 $PREFIX/$LIB_DIR/lib
 install -d -m 0755 $PREFIX/$LIB_DIR/samples
 install -d -m 0755 $PREFIX/$LIB_DIR/templates
+install -d -m 0755 $PREFIX/$ETC_KNOX
 
 tar -xvzf "$BUILD_DIR/knox-${VERSION}.tar.gz" -C "$BUILD_DIR/"
 #cp -ra '/opt/bigtop/build/knox/rpm/BUILD/knox-2.0.0/dist/bin/*' /opt/bigtop/build/knox/rpm/BUILDROOT/knox_3_3_0-2.0.0-1.el8.aarch64//usr/hdp/3.3.0/knox/bin/
@@ -101,3 +106,9 @@ cp -ra $BUILD_DIR/knox-${VERSION}/ext/* ${PREFIX}/${LIB_DIR}/ext/
 cp -ra $BUILD_DIR/knox-${VERSION}/lib/* ${PREFIX}/${LIB_DIR}/lib/
 cp -ra $BUILD_DIR/knox-${VERSION}/samples/* ${PREFIX}/${LIB_DIR}/samples/
 cp -ra $BUILD_DIR/knox-${VERSION}/templates/* ${PREFIX}/${LIB_DIR}/templates/
+
+ln -s $ETC_KNOX/conf $PREFIX/$LIB_DIR/conf
+ln -s /var/log/knox $PREFIX/$LIB_DIR/logs
+ln -s /var/run/knox $PREFIX/$LIB_DIR/pids
+
+cp -ra $BUILD_DIR/knox-${VERSION}/conf/* ${PREFIX}/$ETC_KNOX/conf/
