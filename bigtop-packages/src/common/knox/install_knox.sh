abc2d1cf82671b044a9c2f62d3cd97cb8e001396
huadi <292801371@qq.com>
2025-08-12 11:00:10 +0800

    feat: add knox

--- bigtop-packages/src/common/knox/install_knox.sh
+++ bigtop-packages/src/common/knox/install_knox.sh
@@ -95,11 +95,11 @@ install -d -m 0755 $PREFIX/$LIB_DIR/lib
 install -d -m 0755 $PREFIX/$LIB_DIR/samples
 install -d -m 0755 $PREFIX/$LIB_DIR/templates
 install -d -m 0755 $PREFIX/$ETC_KNOX
+install -d -m 0755 $PREFIX/$ETC_KNOX/conf
 
 tar -xvzf "$BUILD_DIR/knox-${VERSION}.tar.gz" -C "$BUILD_DIR/"
 #cp -ra '/opt/bigtop/build/knox/rpm/BUILD/knox-2.0.0/dist/bin/*' /opt/bigtop/build/knox/rpm/BUILDROOT/knox_3_3_0-2.0.0-1.el8.aarch64//usr/hdp/3.3.0/knox/bin/
 cp -ra $BUILD_DIR/knox-${VERSION}/bin/* ${PREFIX}/${LIB_DIR}/bin/
-cp -ra $BUILD_DIR/knox-${VERSION}/conf/* ${PREFIX}/${LIB_DIR}/conf/
 cp -ra $BUILD_DIR/knox-${VERSION}/data/* ${PREFIX}/${LIB_DIR}/data/
 cp -ra $BUILD_DIR/knox-${VERSION}/dep/* ${PREFIX}/${LIB_DIR}/dep/
 cp -ra $BUILD_DIR/knox-${VERSION}/ext/* ${PREFIX}/${LIB_DIR}/ext/
